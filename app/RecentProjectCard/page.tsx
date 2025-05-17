e3a41bd5dee413d6877284952b57e6d994d189a0
veli <143023447+Velination@users.noreply.github.com>
2025-05-17 20:26:17 +0100

    Update page.tsx
    project card

--- app/RecentProjectCard/page.tsx
+++ app/RecentProjectCard/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           {/* SideBar */}
           <Sidebar />
 
-          <div className={styles.projectGrid}>
-            <Link href="/MyCare">
+           <div className={styles.projectGrid}>
+      <Link href="/MyCare">
               <div>
                 <ProjectCard
                   projectName="MyCard Assistant"
@@ -30,46 +30,46 @@ export default function Home() {
                 />
               </div>
             </Link>
-
-            <Link href="/projects/real-realty">
-              <div>
-                <ProjectCard
-                  projectName="Real Realty Limited"
-                  year="2024"
-                  imageSrc="/images/Shot.png"
-                  title="Real Estate Website"
-                  subtitle="Redesign"
-                  hoverColor="#964B00"
-                />
-              </div>
-            </Link>
-
             <Link href="/tantalizer">
               <div>
                 <ProjectCard
                   projectName="Tantalizer"
                   year="2023"
-                  imageSrc="/images/Shot.png"
+                  imageSrc="/images/Scene 12 1 (4).png"
                   title="Restaurant Website"
                   subtitle="Redesign"
                   hoverColor="#EA8B1B"
                 />
               </div>
             </Link>
-
-            <Link href="/projects/kwats">
+            <Link href="/RockRealty">
+              <div>
+                <ProjectCard
+                  projectName="Real Realty Limited"
+                  year="2024"
+                  imageSrc="/images/Scene 12 1 (5).png"
+                  title="Real Estate Website"
+                  subtitle="Redesign"
+                  hoverColor="#964B00"
+                />
+              </div>
+            </Link>
+            
+            <Link href="/kwats">
               <div>
                 <ProjectCard
                   projectName="Kwats"
                   year="2024"
-                  imageSrc="/images/Shot.png"
+                  imageSrc="/images/Scene 12 1 (6).png"
                   title="Courier Service App for"
                   subtitle="Delivery Service"
                   hoverColor="#90EE90"
                 />
               </div>
             </Link>
-          </div>
+
+      
+    </div>
         </div>
       </div>
     </div>
