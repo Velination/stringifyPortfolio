b5bfd7901ef341439b95d2b73380cf2f78cf8e5c
veli <143023447+Velination@users.noreply.github.com>
2025-05-30 16:20:58 +0100

    Update WriteHeader.tsx
    link

--- component/WriteHeader.tsx
+++ component/WriteHeader.tsx
@@ -1,6 +1,7 @@
 import { Figtree, Bruno_Ace } from 'next/font/google';
 import ProjectCard from "@/component/RecentProjectCard";
 import styles from "@/component/RecentProjectCard.module.css";
+import Link from "next/link";
 
 const figtree = Figtree({
   subsets: ['latin'],
@@ -26,30 +27,42 @@ export default function RecentProjects() {
         <span>DESIGN PROCESS</span>
       </div>
       <div className={styles.projectGrid}>
-      <ProjectCard
-                        projectName="MyCard Assistant"
-                        year="2024"
-                        imageSrc="/images/Shot.png"
-                        title="Care Service App for"
-                        subtitle="Care Professionals & Patients"
-                        hoverColor="#1e90ff"
-                      />
-         <ProjectCard
-                  projectName="Tantalizer"
-                  year="2023"
-                  imageSrc="/images/Scene 12 1 (4).png"
-                  title="Restaurant Website"
-                  subtitle="Redesign"
-                  hoverColor="#EA8B1B"
+       <Link href="/MyCare">
+              <div>
+                <ProjectCard
+                  projectName="MyCard Assistant"
+                  year="2024"
+                  imageSrc="/images/Shot.png"
+                  title="Care Service App for"
+                  subtitle="Care Professionals & Patients"
+                  hoverColor="#1e90ff"
                 />
-       <ProjectCard
+              </div>
+            </Link>
+         <Link href="/projects/real-realty">
+              <div>
+                <ProjectCard
                   projectName="Real Realty Limited"
                   year="2024"
-                  imageSrc="/images/Scene 12 1 (5).png"
+                  imageSrc="/images/Shot.png"
                   title="Real Estate Website"
                   subtitle="Redesign"
                   hoverColor="#964B00"
                 />
+              </div>
+            </Link>
+      <Link href="/tantalizer">
+              <div>
+                <ProjectCard
+                  projectName="Tantalizer"
+                  year="2023"
+                  imageSrc="/images/Shot.png"
+                  title="Restaurant Website"
+                  subtitle="Redesign"
+                  hoverColor="#EA8B1B"
+                />
+              </div>
+            </Link>
 
       
     </div>
