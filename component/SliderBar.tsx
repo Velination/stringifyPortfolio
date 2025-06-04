09755c6d9254e2d92c567adfa81ccdbd5a03bbb0
veli <143023447+Velination@users.noreply.github.com>
2025-06-05 00:53:30 +0100

    Update SliderBar.tsx
    adjustment

--- component/SliderBar.tsx
+++ component/SliderBar.tsx
@@ -2,62 +2,129 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { RxPerson, RxHome, RxDesktop } from "react-icons/rx";
-import { RiFileCopyLine, RiFileCopyFill } from "react-icons/ri";
+import Image from "next/image";
 import styles from "./SlideBar.module.css";
 
 const Sidebar: React.FC = () => {
-  const [isCopied, setIsCopied] = useState(false);
+  const [hoveredIcon, setHoveredIcon] = useState("");
+
+  const [hovered, setHovered] = useState(false);
+  const [copied, setCopied] = useState(false);
 
   const email = "info@rockrealty.com";
 
   const handleCopyClick = () => {
     navigator.clipboard.writeText(email)
       .then(() => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000);
+        setCopied(true);
+        alert("Copied!");
+        setTimeout(() => setCopied(false), 2000);
       })
       .catch((err) => {
-        console.error("Failed to copy:", err);
+        console.error("Copy failed:", err);
       });
   };
+
+  let imageSrc = "/images/Copy.png";
+  if (copied) {
+    imageSrc = "/images/Copied-Hover.png";
+  } else if (hovered) {
+    imageSrc = "/images/Copy-Hover.png";
+  }
+ 
 
   return (
     <div className={styles.sidebar}>
       {/* Vertical Line */}
       <div className={styles.line}></div>
 
-      <Link href="/">
-      <div>
-        <img
-          className={styles.logo}
-          src="/images/LAJlogo3.png" 
+      {/* Logo */}
+      <div className={styles.logo}>
+        <Image
+          src="/images/LAJ Logo 3.png"
           alt="Logo"
           width={24}
           height={24}
         />
       </div>
- 
-      </Link>
-      
+
       {/* Sidebar Icons */}
       <div className={styles.icons}>
-        <Link href='/'><RxHome className={styles.icon} /></Link>
-        <Link href='/AboutPage'><RxPerson className={styles.icon} /></Link>
-        <Link href='/ProjectPage'><RxDesktop className={styles.icon} /></Link>
+
+         <Link href="/">
+    <div
+    role="button"
+  tabIndex={0}
+     onMouseOver={() => setHoveredIcon("home")}
+onMouseOut={() => setHoveredIcon("")}
+className={styles.icon}
+    >
+      <Image
+        src={hoveredIcon === "home" ? "/images/Home-Hover.png" : "/images/Home.png"}
+        alt="Home"
+        width={34}
+        height={34}
+      />
+    </div>
+  </Link>
+         <Link href="/AboutPage">
+    <div
+    role="button"
+  tabIndex={0}
+         onMouseOver={() => setHoveredIcon("about")}
+onMouseOut={() => setHoveredIcon("")}
+className={styles.icon}
+    >
+      <Image
+        src={hoveredIcon === "about" ? "/images/Profile-Hover.png" : "/images/Profile.png"}
+        alt="About"
+        width={24}
+        height={24}
+      />
+    </div>
+  </Link>
+          <Link href="/ProjectPage">
+    <div
+    role="button"
+  tabIndex={0}
+         onMouseOver={() => setHoveredIcon("projects")}
+onMouseOut={() => setHoveredIcon("")}
+className={styles.icon}
+    >
+      <Image
+        src={hoveredIcon === "projects" ? "/images/Monitor-Hover.png" : "/images/Monitor.png"}
+        alt="Projects"
+        width={24}
+        height={24}
+      />
+    </div>
+  </Link>
     
 
       {/* Copy Icon with Text */}
-      <div className={styles.mobileMenuItem} onClick={handleCopyClick}>
-        {isCopied ? (
-          <RiFileCopyFill className={styles.icon} />
-        ) : (
-          <RiFileCopyLine className={styles.icon} />
-        )}
-      </div>
-      </div>
+       
+
+    <div
+      onClick={handleCopyClick}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      style={{ cursor: "pointer" }}
+      className={styles.mobileMenuItem}
+    >
+      <Image
+        src={imageSrc}
+        alt="Copy Email"
+        width={24}
+        height={24}
+      />
     </div>
+  
+        
+      </div>
+      </div>
+    
   );
 };
 
 export default Sidebar;
+
